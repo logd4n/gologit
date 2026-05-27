da8b0e45a0f8fd5a0cdcd10c5f9274985e0f6851
LOG <log.plusplus@gmail.com>
2026-05-27 22:07:59 +0300

    получение логов из БД

--- database/connect.go
+++ database/connect.go
@@ -71,20 +71,3 @@ func ConnectDB() error {
 
 	return nil
 }
-
-func createTable() error {
-	//Таблица logs
-	_, err := dataBase.Exec(`
-	create table if not exists logs (
-	id bigserial primary key,
-	level varchar(10) not null,
-	message text,
-	created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-	)
-	`)
-	if err != nil {
-		return CrTableErr
-	}
-
-	return nil
-}
